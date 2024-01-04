bf8ea4bc5688316042f13038104be3774593179a
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2024-01-04 15:56:50 +0100

    struttura lista fumetti

--- src/components/ComicsList.vue
+++ src/components/ComicsList.vue
@@ -3,7 +3,113 @@ export default {
   name: "ComicsList",
 };
 </script>
-<template></template>
+<template>
+  <div class="container">
+    <div class="card-comic">
+      <img
+        src="https://www.coverbrowser.com/image/action-comics/1-1.jpg"
+        alt=""
+      />
+      <span class="title">Action comics</span>
+    </div>
+    <div class="card-comic">
+      <img
+        src="https://www.coverbrowser.com/image/action-comics/1-1.jpg"
+        alt=""
+      />
+      <span class="title">Action comics</span>
+    </div>
+    <div class="card-comic">
+      <img
+        src="https://www.coverbrowser.com/image/action-comics/1-1.jpg"
+        alt=""
+      />
+      <span class="title">Action comics</span>
+    </div>
+    <div class="card-comic">
+      <img
+        src="https://www.coverbrowser.com/image/action-comics/1-1.jpg"
+        alt=""
+      />
+      <span class="title">Action comics</span>
+    </div>
+    <div class="card-comic">
+      <img
+        src="https://www.coverbrowser.com/image/action-comics/1-1.jpg"
+        alt=""
+      />
+      <span class="title">Action comics</span>
+    </div>
+    <div class="card-comic">
+      <img
+        src="https://www.coverbrowser.com/image/action-comics/1-1.jpg"
+        alt=""
+      />
+      <span class="title">Action comics</span>
+    </div>
+    <div class="card-comic">
+      <img
+        src="https://www.coverbrowser.com/image/action-comics/1-1.jpg"
+        alt=""
+      />
+      <span class="title">Action comics</span>
+    </div>
+    <div class="card-comic">
+      <img
+        src="https://www.coverbrowser.com/image/action-comics/1-1.jpg"
+        alt=""
+      />
+      <span class="title">Action comics</span>
+    </div>
+    <div class="card-comic">
+      <img
+        src="https://www.coverbrowser.com/image/action-comics/1-1.jpg"
+        alt=""
+      />
+      <span class="title">Action comics</span>
+    </div>
+    <div class="card-comic">
+      <img
+        src="https://www.coverbrowser.com/image/action-comics/1-1.jpg"
+        alt=""
+      />
+      <span class="title">Action comics</span>
+    </div>
+    <div class="card-comic">
+      <img
+        src="https://www.coverbrowser.com/image/action-comics/1-1.jpg"
+        alt=""
+      />
+      <span class="title">Action comics</span>
+    </div>
+    <div class="card-comic">
+      <img
+        src="https://www.coverbrowser.com/image/action-comics/1-1.jpg"
+        alt=""
+      />
+      <span class="title">Action comics</span>
+    </div>
+  </div>
+</template>
 <style lang="scss" scoped>
 @use "../styles/partials/_mixins.scss" as *;
+.container {
+  @include center();
+  flex-wrap: wrap;
+  background-color: #1c1c1c;
+
+  .card-comic {
+    @include direction(column);
+    width: calc((100% / 6) - 20px);
+    margin: 20px 10px;
+    img {
+      width: 100%;
+    }
+    span {
+      color: white;
+      font-size: 12px;
+      font-weight: bold;
+    }
+  }
+}
 </style>
